b0445d3a03d314313c12d267f67e382d58f78890
{test} <000.tgtg.000@gmail.com>
2023-01-21 15:19:10 +0900

    全体的なレイアウトを変更

--- zahyo.js
+++ zahyo.js
@@ -1,11 +1,14 @@
-let input_X = document.querySelector("input[name=X]");
-let input_Y = document.querySelector("input[name=Y]");
-let submit = document.querySelector("#set-data");
-let big_angles = document.querySelectorAll("#big_box div div");
-let small_angles = document.querySelectorAll("#small_box div div");
-const formula = document.querySelector("#formula");
+const error_msg = document.querySelector("span");
+const input_X = document.querySelector("input[name=X]");
+const input_Y = document.querySelector("input[name=Y]");
+const input_cell = document.querySelector("input[name=cell]");
+const submit = document.querySelector("#set-data");
+const big_corners = document.querySelectorAll("#big_box div div");
+const small_corners = document.querySelectorAll("#small_box div div");
+const formula = document.querySelector("#formula p");
 const input = document.querySelector('#upload-file');
 const fileRsetbtn = document.querySelector('#reset-file');
+const copy = document.querySelector("#get-formula");
 
 input.addEventListener('change', (e)=>{
   const reader = new FileReader();
@@ -47,11 +50,15 @@ function csvToCoordinate(file, reader){
 function inputToCoordiante(){
   let X = input_X.value.split(',');
   let Y = input_Y.value.split(',');
-  let X1 = Math.min(...X);
-  let X2 = Math.max(...X);
-  let Y1 = Math.min(...Y);
-  let Y2 = Math.max(...Y);
-  return [X1, X2, Y1, Y2];
+  let array = [];
+  if(X.length == 2 && Y.length ==2){
+    let X1 = Math.min(...X);
+    let X2 = Math.max(...X);
+    let Y1 = Math.min(...Y);
+    let Y2 = Math.max(...Y);
+    array = [X1, X2, Y1, Y2];
+  }
+  return array;
 }
 
 function commons(X1,X2,Y1,Y2){
@@ -73,25 +80,114 @@ function commons(X1,X2,Y1,Y2){
   ];
   
   for(let i=0; i<4; i++){
-      big_angles[i].innerText= big_box[i];
+      big_corners[i].innerText= big_box[i];
   }
   for(let i=0; i<4; i++){
-      small_angles[i].innerText= small_box[i];
+      small_corners[i].innerText= small_box[i];
   }
 
-  let x_red = "<span class='red'>x</span>";
-  let y_red = "<span class='red'>y</span>";
-  formula.innerHTML=`IF(AND(${x_red}>${x1}, ${y_red}>${y1}), IF(AND(${x_red}<${x2}, ${y_red}<${y2}), Center, Edge), Edge)`
+  let cells = input_cell.value.split(',');
+  // let y_cell = input_cell.value.split(',');
+  formula.innerHTML=`IF(AND(${cells[0]}>${x1}, ${cells[1]}>${y1}), IF(AND(${cells[0]}<${x2}, ${cells[2]}<${y2}), Center, Edge), Edge)`
   
   input_X.value = "";
   input_Y.value = "";
+  input_cell.value ="";
 }
 
-submit.addEventListener('click', (e)=>{
-  if(input_X.value){
+function error(msg){
+  error_msg.innerText = msg;
+  error_msg.parentElement.animate(
+    {
+      // background: ["rgb(207, 40, 40)", "rgb(241, 17, 55)", "rgb(207, 40, 40)"],
+      transform: [
+        "translate(0px,0px)",
+        "translate(3px, 0px)",
+        "translate(0px,0px)",
+        "translate(-3px, 0px)",
+        "translate(0px, 0px)",
+      ],
+      easing: 'linear',
+      // easing: 'ease-in-out',
+    },
+    {
+      iterations: 10,
+      duration: 100,
+    }
+  );
+}
+
+
+function validation(X,Y,cells){
+  error_msg.innerText="";
+  if(input.value){
+    if (input.value.split('.').pop() == "csv" ){
+      if( cells.value){
+        if(cells.length != 2){
+          error("参照するセルは2つ必要です");
+          return false
+        } else {
+          return true;
+        }
+      } else {
+        error("値の入力、CSVファイルのアップロードのいずれかが必要です。");
+        return false;
+      }
+    } else {
+      error("利用できるファイル形式はcsvのみです。")
+      return false;
+    }    
+  } else if (input_X.value && input_Y.value){
+    if(X.length==2 && Y.length==2){
+      if(input_cell.value){
+        if(cells.length==2){
+          return true;
+        } else {
+          error("参照するセルは2つ必要です。");
+          return false;
+        }
+      } else {
+        error("参照するセルを指定してください。");
+        return false;
+      }
+    } else{
+      error("x,yはそれぞれ2つの値が必要です。");
+      return false;
+    }
+  } else {
+    error("値の入力、CSVファイルのアップロードのいずれかが必要です。");
+    return false;
+  }
+} 
+
+// function validation(X,Y,cells){
+//   let check = true
+//   if(!((X && Y) || input.value)){
+//     error("値の入力、CSVファイルのアップロードのいずれかが必要です。");
+//     check = false;
+//   }else if ( !(X.length==2 && Y.length==2) ){
+//     error("")
+//   } else if (!input_cell.value){
+//     error("参照するセルを指定してください");
+//     check = false;
+//   } else if (input_cell.value.split(',').length != 2){
+//     error("")
+//   }
+//   return check;
+// }
+
+submit.addEventListener('click', ()=>{
+  let X = input_X.value.split(',');
+  let Y = input_Y.value.split(',');
+  let cells = input_cell.value.split(',');
+  if(validation(X,Y,cells)){
     let [X1, X2, Y1, Y2] = inputToCoordiante();
     commons(X1,X2,Y1,Y2);
   }
 })
 
-
+copy.addEventListener("click",()=>{
+  navigator.clipboard.writeText(formula.textContent);
+  copy.innerText="コピーしました";
+  setTimeout(() => copy.innerText = 'コピー', 700);
+})
